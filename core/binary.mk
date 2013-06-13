dc16fd394fa382db8926fb89ad871a412d9bfe17
Fredrik Karlsson <fkrlsn@gmail.com>
2013-06-13 15:40:21 +0200

    allow local header overrides
    Change-Id: Id51395f1a386e7188db41a00a51c263f683f5b8e

--- core/binary.mk
+++ core/binary.mk
@@ -553,6 +553,10 @@ all_objects := \
     $(proto_generated_objects) \
     $(addprefix $(TOPDIR)$(LOCAL_PATH)/,$(LOCAL_PREBUILT_OBJ_FILES))
 
+ifneq ($(TARGET_SPECIFIC_HEADER_PATH),)
+LOCAL_C_INCLUDES := $(TOPDIR)$(TARGET_SPECIFIC_HEADER_PATH) $(LOCAL_C_INCLUDES)
+endif
+
 LOCAL_C_INCLUDES += $(TOPDIR)$(LOCAL_PATH) $(intermediates)
 
 ifndef LOCAL_SDK_VERSION
